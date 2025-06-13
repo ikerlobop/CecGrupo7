1b4d1b06a1938ec1e49cc1c8ff17f2041dce28bf
Iker Lobo Perez <ikerlobop@gmail.com>
2025-06-13 09:06:40 +0200

    Declarar como abstracta la clase Expedicionario, crear constructor y getters.

--- src/Expedicionario.java
+++ src/Expedicionario.java
@@ -1,2 +1,21 @@
-public class Expedicionario {
+public abstract class Expedicionario {
+    protected int id;
+    protected String nombre, email, telefono;
+
+    public Expedicionario(int id, String nombre, String email, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public abstract void mostrarInfo();
 }
