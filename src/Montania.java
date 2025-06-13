b95e0d66595dd5c7db25fcb682c2b6f054694d33
Iker Lobo Perez <ikerlobop@gmail.com>
2025-06-13 10:05:58 +0200

    añadir clases de Montaña y Participación

---
+++ src/Montania.java
@@ -0,0 +1,13 @@
+public class Montania {
+    int id;
+    String nombre;
+    float altura;
+    String dificultad;
+
+    public Montania(int id, String nombre, float altura, String dificultad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.altura = altura;
+        this.dificultad = dificultad;
+    }
+}
---
+++ src/Participacion.java
@@ -0,0 +1,9 @@
+public class Participacion {
+    Expedicionario expedicionario;
+    String rol;
+
+    public Participacion(Expedicionario e, String rol) {
+        this.expedicionario = e;
+        this.rol = rol;
+    }
+}
